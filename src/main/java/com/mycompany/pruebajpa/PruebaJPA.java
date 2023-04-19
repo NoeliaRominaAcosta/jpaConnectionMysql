fbd21b996e9394a7be77fb0b33e3ad758f23e9b7
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2023-04-19 19:42:02 -0300

    mysql driver working

---
+++ src/main/java/com/mycompany/pruebajpa/PruebaJPA.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebajpa;
+
+import persistencia.ControladoraPersistencia;
+
+/**
+ *
+ * @author CONECTIA BA
+ */
+public class PruebaJPA {
+
+    public static void main(String[] args) {
+        ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+    }
+}
